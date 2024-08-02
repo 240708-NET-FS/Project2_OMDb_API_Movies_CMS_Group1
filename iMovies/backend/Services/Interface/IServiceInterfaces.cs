cd11baabf2a56e37e87fd8c65f07be25675fad03
TimStDennis <87658836+TimStDennis@users.noreply.github.com>
2024-08-02 17:52:44 -0400

    Creation of Services
    Basic setup for Services

---
+++ iMovies/backend/Services/Interface/IServiceInterfaces.cs
@@ -0,0 +1,23 @@
+using OMDBProject.DTO;
+
+namespace OMDBProject.Services;
+
+public interface IFollowersService
+{
+
+}
+
+public interface ILikesService
+{
+
+}
+
+public interface IUserMoviesService
+{
+
+}
+
+public interface IUsersService
+{
+
+}
