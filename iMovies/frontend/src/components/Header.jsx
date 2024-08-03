f2239bde19c64a49c6b8679850bf084cd906d339
Unnati Patel <unnati314@revature.net>
2024-08-03 15:35:39 -0400

    added header

--- iMovies/frontend/src/components/Header.jsx
+++ iMovies/frontend/src/components/Header.jsx
@@ -2,28 +2,44 @@ import React from "react"
 import { Link, NavLink } from "react-router-dom"
 
 export default function Header() {
-    const activeStyles = {
-        fontWeight: "bold",
-        textDecoration: "underline",
-        color: "#161616"
-    }
-    
-    return (
-        <header>
-            <nav>
-                <NavLink 
-                    to="/"
-                    style={({isActive}) => isActive ? activeStyles : null}
+  const activeStyles = {
+    fontWeight: "bold",
+    textDecoration: "underline",
+    color: "#161616"
+  }
+
+  return (
+    <header>
+      <div className="stickable-header-w">
+        <div id="site-header-menu" className="site-header-menu">
+          <div className="site-header-menu-inner ttm-stickable-header">
+            <div className="container">
+              <div className="site-branding">
+                <h2 className="logo-text">OMDB</h2>
+
+              </div>
+              <ul className="nav-menu"> <li className="nav-menu-li"><NavLink
+                to="/"
+                style={({ isActive }) => isActive ? activeStyles : null}
+              >
+                Home
+              </NavLink></li>
+                <li className="nav-menu-li"> <NavLink
+                  to="signup"
+                  style={({ isActive }) => isActive ? activeStyles : null}
                 >
-                    Home
-                </NavLink>
-                <NavLink 
-                    to="signup"
-                    style={({isActive}) => isActive ? activeStyles : null}
+                  SignUp
+                </NavLink></li>
+                <li className="nav-menu-li"> <NavLink
+                  to="signup"
+                  style={({ isActive }) => isActive ? activeStyles : null}
                 >
-                    SignUp
-                </NavLink>
-            </nav>
-        </header>
-    )
+                  Login
+                </NavLink></li></ul>
+            </div>
+          </div>
+        </div>
+      </div>
+    </header>
+  )
 }
