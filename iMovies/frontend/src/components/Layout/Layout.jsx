c66734d602f4e9ff05682cb3b739b859185e9a36
Tomy Romero <tomyfletcher99@hotmail.com>
2024-08-03 20:55:36 -0400

    Modular css structure

--- iMovies/frontend/src/components/Layout/Layout.jsx
+++ iMovies/frontend/src/components/Layout/Layout.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import { Outlet, Link } from 'react-router-dom';
-import Header from './Header';
+import { Outlet } from 'react-router-dom';
+import Header from '../Header/Header';
 
 function Layout() {
   return (
--- iMovies/frontend/src/main.jsx
+++ iMovies/frontend/src/main.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Home from "./pages/Home"
-import SignUp from './pages/SignUp';
-import Layout from './components/Layout';
+import Home from "./pages/Home/Home"
+import SignUp from './pages/Signup/Signup';
+import Layout from './components/Layout/Layout';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
